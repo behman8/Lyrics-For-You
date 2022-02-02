0238594d1e3753be911ed4a3c2f450c82c0e448c
Branden <brandenehman45@gmail.com>
2022-02-02 12:43:04 -0700

    Connected all tables within the models, gave routes for logging in and out, and gave methods to both the users controller and sessions controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :ok
+        else
+            render json: { errors: "Not authorized."}, status: :unauthorized
+        end
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :songs
+    has_many :favorites
+    has_many :songs, through: :favorites
+    valitdates :username, presence: true, length: { minimum: 5 }, uniqueness: true
 end
